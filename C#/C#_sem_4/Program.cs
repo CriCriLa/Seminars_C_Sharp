c32aebc35e1d8f6e3cfffb3ec53aa5959ec6f84a
Larisa <cricri.lv@gmail.com>
2023-08-22 15:51:28 +0300

    Task 26

--- C#/C#_sem_4/Program.cs
+++ C#/C#_sem_4/Program.cs
@@ -1,21 +1,42 @@
 ﻿// Задача 24. Напишите программу, которая принимает на вход число А 
 // и выдает сумму чисел от 1 до А.
 
+// Console.Clear();
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int GetSumNumber()
+// {
+//     int sum = 0;
+   
+//     for (int count=1; number >= count; count++)
+//     {
+//         sum = sum + count;
+//     }
+//     return  sum;
+// }
+
+// int result = GetSumNumber();
+// Console.WriteLine(result);
+
+// Задача 26. Принимает на вход число и выдает количестко цифр в числе. 
+
 Console.Clear();
 
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int GetSumNumber()
+int GetCountNumber()
 {
-    int sum = 0;
+   int count = 0;
    
-    for (int count=1; number >= count; count++)
-    {
-        sum = sum + count;
-    }
-    return  sum;
-}
+     while (number>0)
+     {
+         number = number/10;
+         count++;
+     }
+     return  count;
+ }
 
-int result = GetSumNumber();
-Console.WriteLine(result);
+ Console.WriteLine(GetCountNumber());
