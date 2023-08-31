24be0b50175022d5c8e3b70dfa434377b8a833c0
Larisa <cricri.lv@gmail.com>
2023-08-31 15:51:08 +0300

    task 32

--- C#/C#_sem5/Program.cs
+++ C#/C#_sem5/Program.cs
@@ -3,27 +3,48 @@
 // элементов массива. НАпример в массиве [3, 9, -8, 1, 0, -7, 2, -1, 8, -3, -1, 6]
 // сумма положительных чисел равна 29, сумма отрицательных равна -20.
 
+// Console.Clear();
+// int arrayLength = 12;
+// int[] array = new int[arrayLength];
+// int positiveNumber = 0;
+// int negNumber = 0;
+
+// for (int i=0; i< array.Length; i++)
+// {
+//     array[i] = new Random().Next(-9,10);
+
+//     if (array[i]>0)
+//     {
+//         // positiveNumber = positiveNumber + array[i];
+//         positiveNumber += array[i];
+//     }
+//     else
+//     {
+//         negNumber+=array[i];
+//     }
+
+//     Console.Write (array[i]+ " ");
+// }
+// Console.WriteLine();
+// Console.WriteLine($"Сумма положительных: {positiveNumber} и отрицательных: {negNumber};");
+
+// Задача 32. Заменить элементы массива: положительные 
+// на соответствующие отрицательные и наоборот.
+
 Console.Clear();
-int arrayLength = 12;
+int arrayLength = 10;
 int[] array = new int[arrayLength];
-int positiveNumber = 0;
-int negNumber = 0;
 
 for (int i=0; i< array.Length; i++)
 {
     array[i] = new Random().Next(-9,10);
-
-    if (array[i]>0)
-    {
-        // positiveNumber = positiveNumber + array[i];
-        positiveNumber += array[i];
-    }
-    else
-    {
-        negNumber+=array[i];
-    }
-
     Console.Write (array[i]+ " ");
 }
 Console.WriteLine();
-Console.WriteLine($"Сумма положительных: {positiveNumber} и отрицательных: {negNumber};");
+
+for (int i=0; i< array.Length; i++)
+{
+    array[i] = array[i] * -1;
+    Console.Write (array[i]+ " ");
+}
+Console.WriteLine();
