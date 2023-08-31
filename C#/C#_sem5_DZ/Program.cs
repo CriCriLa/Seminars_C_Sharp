28ddcf09bd2bf77bb82068ccf0762225041c9d5a
Larisa <cricri.lv@gmail.com>
2023-08-31 16:58:21 +0300

    Task 36

--- C#/C#_sem5_DZ/Program.cs
+++ C#/C#_sem5_DZ/Program.cs
@@ -2,19 +2,39 @@
 // трехзначными числами. Напишите программу, которая покажет количество
 // четных чисел в массиве.
 
+// Console.Clear();
+// int arrayLength = 10;
+// int[]  array = new int[arrayLength];
+// int sumPol = 0;
+
+// for (int i= 0; i<array.Length; i++)
+// {
+//     array[i] = new Random().Next(100,1000);
+//     Console.Write(array[i] + " ");
+//     if (array[i]%2==0)
+//     {
+//         sumPol++;
+//     }
+// }
+// Console.WriteLine();
+// Console.WriteLine(sumPol + " положительных чисел");
+
+// Задача 36. Задайте одномерный массив, заполненный случайными числами.
+// Найти сумму элементов, стоящих на нечетных позициях.
+
 Console.Clear();
-int arrayLength = 10;
+int arrayLength = 5;
 int[]  array = new int[arrayLength];
-int sumPol = 0;
+int sum = 0;
 
 for (int i= 0; i<array.Length; i++)
 {
-    array[i] = new Random().Next(100,1000);
+    array[i] = new Random().Next(99);
     Console.Write(array[i] + " ");
-    if (array[i]%2==0)
+    if (i%2==1)
     {
-        sumPol++;
+        sum = sum+array[i];
     }
 }
 Console.WriteLine();
-Console.WriteLine(sumPol + " положительных чисел");
+Console.WriteLine("Сумма элементов: "+ sum);
