05d8012c03cb476ee685b7c47ea475814017ed40
Larisa <cricri.lv@gmail.com>
2023-09-05 13:40:09 +0300

    task 42

--- C#/C#_sem6/Program.cs
+++ C#/C#_sem6/Program.cs
@@ -3,20 +3,40 @@
 // (Теорема о неравенстве треугольника: каждая сторона треугольника
 // меньше суммы двух других сторон)
 
-Console.Write("Введите число:");
-int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число:");
+// int num1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите число:");
-int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число:");
+// int num2 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите число:");
-int num3 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число:");
+// int num3 = Convert.ToInt32(Console.ReadLine());
 
-if ((num1<num2+num3)&&(num2<num1+num3)&&(num3<num1+num2))
+// if ((num1<num2+num3)&&(num2<num1+num3)&&(num3<num1+num2))
+// {
+//     Console.WriteLine("Существует.");
+// }
+// else
+// {
+//     Console.WriteLine("Такого треугольника не существует.");
+// }
+
+// Задача 42. Напишите программу, которая преобразует 
+// десятичное число в двоичное.
+
+Console.Clear();
+
+Console.Write("Введите десятичное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+string result ="";
+
+while (num>0)
 {
-    Console.WriteLine("Существует.");
+    result = num%2+result;
+    num= num/2;
 }
-else
-{
-    Console.WriteLine("Такого треугольника не существует.");
-}
+
+Console.WriteLine(result);
+
+
+
