ffd242b7134de5e0972964730926468ad04d7db6
Larisa <cricri.lv@gmail.com>
2023-09-05 14:49:31 +0300

    task 41

--- C#/C#_sem6/Program.cs
+++ C#/C#_sem6/Program.cs
@@ -77,3 +77,4 @@ for (int i =0; i<array.Length; i++)
 {
     Console.Write(array[i]+ " ");
 }
+
--- C#/C#_sem6_DZ/Program.cs
+++ C#/C#_sem6_DZ/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿// Задача 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввел пользователь.
+Console.Clear();
+
+Console.Write("Сколько чисел вы хотите ввести? ");
+int numM = Convert.ToInt32(Console.ReadLine());
+// int[] array= new int [numM];
+int result = 0;
+
+for (int i= 0; i<numM; i++)
+{
+    Console.Write("Введите число: ");
+    // array[i]=Convert.ToInt32(Console.ReadLine());
+    // if (array[i]>0) result++;
+    if (Convert.ToInt32(Console.ReadLine())>0) result++;
+}
+
+Console.WriteLine($"Введено положительных чисел: {result}.");
