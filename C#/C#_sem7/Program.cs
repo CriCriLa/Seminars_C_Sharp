d5c63c02acaab4f6b9e7308fda28b18d1b48eaf9
Larisa <cricri.lv@gmail.com>
2023-09-05 16:25:44 +0300

    Task 46

--- C#/C#_sem7/Program.cs
+++ C#/C#_sem7/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 46. Задать двумерный массив размером m*n, 
+// заполненный случайными целыми числами.
+
+Console.Clear();
+
+Console.Write ("Введите первое число: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write ("Введите второе число: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix=new int[rows,columns];
+
+for (int i= 0; i<matrix.GetLength(0); i++)
+{
+    for (int j=0; j<matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(0,10);
+        Console.Write (matrix [i,j]+ " ");
+    }
+    Console.WriteLine();
+}
