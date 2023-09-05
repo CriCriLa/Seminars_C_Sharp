fd5dbb8423c47470ac029493be1508478c1ec937
Larisa <cricri.lv@gmail.com>
2023-09-05 13:51:39 +0300

    Task 44

--- C#/C#_sem6/Program.cs
+++ C#/C#_sem6/Program.cs
@@ -24,19 +24,38 @@
 // Задача 42. Напишите программу, которая преобразует 
 // десятичное число в двоичное.
 
+// Console.Clear();
+
+// Console.Write("Введите десятичное число:");
+// int num = Convert.ToInt32(Console.ReadLine());
+// string result ="";
+
+// while (num>0)
+// {
+//     result = num%2+result;
+//     num= num/2;
+// }
+
+// Console.WriteLine(result);
+
+// Задача 44. Не использую рекурсию, выведите первые 
+// N чисел  Фибоначчи. Первые два числа 0 и 1.
+
+
 Console.Clear();
 
-Console.Write("Введите десятичное число:");
+Console.Write("Введите число:");
 int num = Convert.ToInt32(Console.ReadLine());
-string result ="";
 
-while (num>0)
+int firstNum = 0;
+int secondNum = 1;
+
+Console.Write(firstNum + " "+secondNum + " ");
+for (int i = 3; i<= num; i++)
 {
-    result = num%2+result;
-    num= num/2;
+    int newNum = firstNum+secondNum;
+    Console.Write(newNum+" ");
+    firstNum = secondNum;
+    secondNum = newNum;
 }
-
-Console.WriteLine(result);
-
-
 
