4edc64c30668eeb6c25b8e995f701f0ed7febe8d
Larisa <cricri.lv@gmail.com>
2023-09-07 11:30:01 +0300

    Task52

--- C#/C#_sem7_DZ/Program.cs
+++ C#/C#_sem7_DZ/Program.cs
@@ -28,28 +28,66 @@
 // элемента в двумерном массиве, и возвращает значение этого элемента
 // или же указание, что такого элемента нет.
 
+// Console.Clear();
+
+// int [,] matrix = {
+//     {1, 4, 7, 2},
+//     {5, 9, 2, 3},
+//     {8, 4, 2, 4}
+// };
+
+// int Prompt(string message) // метод запроса числа от пользователя
+// {
+//     Console.Write(message);
+//     int num = Convert.ToInt32(Console.ReadLine()); 
+//     return num;
+// }
+// int row = Prompt("Укажите ряд: ");
+// int column = Prompt("Укажите колонку: ");
+
+// if ((row >= matrix.GetLength(0)) || (column >= matrix.GetLength(1)))
+// {
+//     Console.WriteLine("Такого числа в массиве нет.");
+// }
+// else 
+// {
+//     Console.WriteLine($"Запрашиваемый элемент: {matrix[row, column]}");
+// }
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее
+// арифметическое элементов в каждом столбце.
+
 Console.Clear();
 
-int [,] matrix = {
-    {1, 4, 7, 2},
-    {5, 9, 2, 3},
-    {8, 4, 2, 4}
-};
+int rows = 3;
+int columns = 4;
 
-int Prompt(string message) // метод запроса числа от пользователя
-{
-    Console.Write(message);
-    int num = Convert.ToInt32(Console.ReadLine()); 
-    return num;
-}
-int row = Prompt("Укажите ряд: ");
-int column = Prompt("Укажите колонку: ");
+int [,] matrix = new int[rows,columns];
 
-if ((row >= matrix.GetLength(0)) || (column >= matrix.GetLength(1)))
+double[] averageArray = new double[matrix.GetLength(1)];
+
+for (int i=0; i<matrix.GetLength(0); i++)
 {
-    Console.WriteLine("Такого числа в массиве нет.");
+    for (int j= 0; j<matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(1,10);
+        Console.Write($"{matrix[i,j]}  ");
+    }
+    Console.WriteLine();
 }
-else 
+
+for (int i=0; i<matrix.GetLength(1); i++)
 {
-    Console.WriteLine($"Запрашиваемый элемент: {matrix[row, column]}");
+    for (int j=0; j<matrix.GetLength(0); j++)
+    {
+        averageArray[i] = averageArray[i]+matrix[j,i];
+    }
+    averageArray[i] = averageArray[i]/matrix.GetLength(0);
 }
+
+Console.Write($"Среднее арифметическое каждого столбца: ");
+for (int i=0; i<averageArray.Length; i++)
+{
+    Console.Write($"{Math.Round(averageArray[i], 1)}  ");
+}
+Console.WriteLine();
