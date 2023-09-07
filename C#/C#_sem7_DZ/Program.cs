2094c4966796adf813064d04cd9b3640ae808912
Larisa <cricri.lv@gmail.com>
2023-09-07 10:50:44 +0300

    Task 47

--- C#/C#_sem7_DZ/Program.cs
+++ C#/C#_sem7_DZ/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 47. Задать двумерный массив размером m*n, 
+// заполненный случайными вещественными числами.
+
+Console.Clear();
+
+int Prompt(string message) // метод запроса числа от пользователя
+{
+    Console.Write(message);
+    int num = Convert.ToInt32(Console.ReadLine()); 
+    return num;
+}
+int rows = Prompt("Введите количество рядов: ");
+int columns = Prompt("Введите количество столбцов: ");
+
+double[,] matrix = new double[rows, columns];
+
+for (int i=0; i<matrix.GetLength(0); i++)
+{
+    for (int j = 0; j<matrix.GetLength(1); j++) 
+    {
+        matrix[i,j] = Math.Round(new Random().NextDouble()*10, 2);
+        Console.Write($"{matrix[i,j]}  ");
+    }
+    Console.WriteLine();
+}
