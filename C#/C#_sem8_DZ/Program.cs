19675d42bf588e967170a5298c956186259528a7
Larisa <cricri.lv@gmail.com>
2023-09-29 13:41:46 +0300

    task 63, 65, 67 & 69

--- C#/C#_sem8_DZ/Program.cs
+++ C#/C#_sem8_DZ/Program.cs
@@ -1,1 +1,86 @@
-﻿
+﻿// Задача 54. Задайте двухмерный массив. Напишите программу, которая упорядочит 
+// по убыванию элементы каждой строки двумерного массива.
+
+Console.Clear();
+
+Console.Write("Введите количество строчек:");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество колонок:");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int [rows, columns];
+
+void FillMatrix()
+{
+    for (int i = 0; i<matrix.GetLength(0); i++)
+    {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(0,10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i<array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] GetRowAsaList(int numOfRow);
+{
+    int[] rowAsaList = new int[matrix.GetLength(1)];
+    for (int i=0; i<matrix.GetLength(1); i++)
+    {
+        rowAsaList[i] = matrix[numOfRow,i];
+    }
+    return rowAsaList;
+}
+
+void SortArrayMaxMin(int[] array)
+{
+    int temp = 0;
+    for (int j=0; j<array.Length-2; j++)
+    {
+        for (int i= 1; i<array.Length-j; i++) 
+        {
+            if (array[i]>array[i-1])
+            {
+                temp = array[i-1];
+                array[i-1] = array[i];
+                array[i] = temp;
+            }
+        }
+    }
+}
+
+void PutRowInMatrix(int[] array, int[,] matrix, int numRow)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+        matrix[numRow,i] = array[i];
+    }
+}
+
+void ChangeMatrix()
+{
+    int[] tempRow = new int[matrix.GetLength(1)];
+    for (int i=0; i<matrix.GetLength(0); i++)
+    {
+        tempRow = GetRowAsaList (i);
+        SortArrayMaxMin(tempRow);
+        PutRowInMatrix(tempRow, matrix, i);
+    }    
+}
+
+FillMatrix();
+PrintMatrix(matrix);
+Console.WriteLine ("_______________");
+ChangeMatrix();
+PrintMatrix(matrix);
--- C#/C#_sem9/Program.cs
+++ C#/C#_sem9/Program.cs
@@ -1,2 +1,93 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 63. Задайте значение N. Программа должна вывести 
+// все натуральные числа в промежутке от 1 до N
+
+// Console.Clear();   
+// Console.Write("Введите число: "); 
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int count = 1;
+
+// string PrintNumber (int start, int end)
+// {
+//     if (start == end)
+//     {
+//         return start.ToString();
+//     }
+//     return (start+ " " + PrintNumber(start+1, end));
+// }
+
+// Console.WriteLine(PrintNumber(count, num));
+
+
+// Задача 65. Задать числа M и N. Вывести все натуральные числа от M до N.
+
+// Console.Clear();   
+// Console.Write("Введите первое число: "); 
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: "); 
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// string PrintNumber (int start, int end)
+// {
+//     if (start == end)
+//     {
+//         return start.ToString();
+//     }
+//     return (start+ " " + PrintNumber(start+1, end));
+// }
+
+// Console.WriteLine(PrintNumber(num1, num2));
+
+// Задача 67. Написать программу которая считает сумму цифр
+//  в заданном пользователе числе.
+
+// Console.Clear();   
+// Console.Write("Введите число: "); 
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+
+// int SumNumber (int start)
+// {
+//     if (start<0)
+//     {
+//         start = start*-1;
+//     }
+//     if (start <= 0)
+//     {
+//         return 0;
+//     }
+//     return (start%10 + SumNumber(start/10));
+// }
+
+// Console.WriteLine(SumNumber(num1));
+
+// Задача 69. Принимает два числа A и В  и возводит 
+// число А в целую степень числа В с помощью рекурсии.
+
+
+Console.Clear();   
+Console.Write("Введите первое число: "); 
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: "); 
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int GetPow (int start, int rank)
+{
+    if (rank== 0)
+    {
+        return 1;
+    }
+    if (rank ==1)
+    {
+        return start;
+    }
+    if (start <= 0)
+    {
+        return 0;
+    }
+    return (start * GetPow(start, rank-1));
+}
+
+Console.WriteLine(GetPow(num1, num2));
